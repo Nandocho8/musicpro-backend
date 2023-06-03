2859b7821414e562487445db931f19ad81d98855
AlejandroFerrera <aferrerah1421@gmail.com>
2023-06-03 15:29:44 -0400

    Add static route

--- musicpro/musicpro/settings.py
+++ musicpro/musicpro/settings.py
@@ -16,6 +16,9 @@ from keys import DATA_HOST, KEY, DATA_USER, DATA_NAME, DATA_PASSWORD
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
