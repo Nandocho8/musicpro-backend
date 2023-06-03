1c9166976a2625012054639e4af433276bf245cf
AlejandroFerrera <aferrerah1421@gmail.com>
2023-06-03 15:23:42 -0400

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+	# Must consider everything related to the rebase process
+	# and any extra step for this proyect.
+
+	# Activate Virtualenv
+	source ../bin/activate
+	# update pip
+	printf "\n > Updating pip ...\n\n"
+	pip install --upgrade pip
+	# install new dependencies
+	printf "\n > Updating python libraries ...\n\n"
+	pip install -r requirements.txt
+
+	printf "\n > Updating django models ...\n\n"
+	python manage.py migrate
+	
+	# collect statics
+	printf "\n > Updating static files ...\n\n"
+	echo yes | python manage.py collectstatic
+
+	# Deactivate Virtualenv
+	deactivate
