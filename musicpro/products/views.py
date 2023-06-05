04649a98ae8b297d23772c5d6f6766e71a1d890f
AlejandroFerrera <aferrerah1421@gmail.com>
2023-06-04 20:46:03 -0400

    Test discord webhook

--- musicpro/products/views.py
+++ musicpro/products/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def hello_world(request):
-    return JsonResponse({'message': 'Hello, world!'})
+    return JsonResponse({'message': 'Hello, world! with discord webhook'})
