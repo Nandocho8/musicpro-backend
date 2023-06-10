a685f908fef572a194928e3463b3e8d4015cd37e
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-06-10 02:28:25 -0400

    endpoint product url

--- musicpro/products/urls.py
+++ musicpro/products/urls.py
@@ -8,5 +8,6 @@ router.register('api/type', Type_Viewset, 'type')
 router.register('api/category', Category_Viewset, 'category')
 router.register('api/subcategory', Subcategory_Viewset, 'subcategory')
 router.register('api/brand', Brand_Viewset, 'brand')
+router.register('api/product', Product_Viewset, 'product')
 
 urlpatterns = router.urls
