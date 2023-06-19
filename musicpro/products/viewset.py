09b95200de9e7a8a4f3d8c86371b1b903089972f
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-06-18 21:26:02 -0400

    allow only get-option methods

--- musicpro/products/viewset.py
+++ musicpro/products/viewset.py
@@ -8,22 +8,27 @@ class Type_Viewset(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = Type_Serializers
+    http_method_names = ['get', 'head']
 
 class Category_Viewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = Category_Serializers
+    http_method_names = ['get', 'head']
+
 
 class Subcategory_Viewset(viewsets.ModelViewSet):
     queryset = Subcategory.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = Subcategory_Serializers
+    http_method_names = ['get', 'head']
 
 
 class Brand_Viewset(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = Brand_Serializers
+    http_method_names = ['get', 'head']
 
 class Product_Viewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -31,3 +36,4 @@ class Product_Viewset(viewsets.ModelViewSet):
     serializer_class = Product_Serializers
     filter_backends = [DjangoFilterBackend]
     filterset_class = Product_Filter
+    http_method_names = ['get', 'head']
