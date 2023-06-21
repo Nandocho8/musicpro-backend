ed306538df2153db9d353e26ad47d9be72f04a5b
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-06-21 05:25:17 -0400

    add stock urls

--- musicpro/products/urls.py
+++ musicpro/products/urls.py
@@ -9,5 +9,6 @@ router.register('api/category', Category_Viewset, 'category')
 router.register('api/subcategory', Subcategory_Viewset, 'subcategory')
 router.register('api/brand', Brand_Viewset, 'brand')
 router.register('api/product', Product_Viewset, 'product')
+router.register('api/stock', Stock_Viewset, 'stock')
 
 urlpatterns = router.urls
