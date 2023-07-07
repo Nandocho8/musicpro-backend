5e3221c50c1e4d3b5b967a4ea5d1a733ad865eae
Nandocho8 <ccontrerasvergara@hotmail.cl>
2023-07-06 23:47:03 -0400

    fix (settings): comment statics file

--- musicpro/musicpro/settings.py
+++ musicpro/musicpro/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 # REST_FRAMEWORK = {
